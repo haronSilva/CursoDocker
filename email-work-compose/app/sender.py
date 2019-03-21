343f6a8f402cb7bd7f1f02a691c2ac5ffad638bf
haron.costa <haron.costa@softvaro.com.br>
2019-03-21 13:25:41 -0300

    Escalando o worker, infelizmente a versão do meu compose não deixa

--- email-work-compose/app/sender.py
+++ email-work-compose/app/sender.py
@@ -1,12 +1,38 @@
-from bottle import route, run, request
+import psycopg2
+import redis
+import json
+from bottle import Bottle, request
+#Datasource Name
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+        DSN = 'dbname=email_sender user=postgres host=db'
+        self.conn = psycopg2.connect(DSN)
 
-@route('/', method='POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
-    return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
-        assunto, mensagem
-    )
+    def register_message(self, assunto, mensagem):        
+        SQL= 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+
+        print('Mensagem Registradas !')
+
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+            assunto, mensagem
+        )
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
